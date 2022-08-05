bc294169ecfb3f6ea43b65f84b7911dba7203e8a
Alisher <alishertogoyev682@gmail.com>
2022-08-05 22:33:55 +0300

    componnets file create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import Header from "./components/header/Header";
 const App=()=>{
     return (
-        <div>
-            App
-        </div>
+        <>
+          <Header/>
+        </>
     )
 }
 
